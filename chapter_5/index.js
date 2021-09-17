3c3c8b06bcb0524d7fa95d19302b174f726f324d
Wojciech Sobczak <wsobczak@gmail.com>
2021-09-17 14:37:28 +0200

    Chapter 5 Layouty, ejs, express-ejs-layouts

--- chapter_5/index.js
+++ chapter_5/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
 const port = 3000;
-
+const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'));
+// set layout
+app.use(ejsLayouts);
+app.set('layout', './layouts/main');
 
 app.get('/', (req, res) => {
   // res.send('Hello express');
-  res.sendFile(path.join(__dirname + '/views/home.html'));
+  // res.sendFile(path.join(__dirname + '/views/home.html'));
+  res.render('pages/home', {
+    title: 'Strona główna'
+  });
 });
 
 app.get('/firmy/:name', (req, res) => {
@@ -17,12 +27,23 @@ app.get('/firmy/:name', (req, res) => {
   ];
 
   const company = companies.find(x => x.slug === name);
-  if (company) {
-    res.send(`Nazwa firmy: ${company.name}`);
-  } else {
-    res.send(`Nie ma takiej firmy`);
-  }
+  // if (company) {
+  //   res.send(`Nazwa firmy: ${company?.name}`);
+  // } else {
+  //   res.send(`Nie ma takiej firmy`);
+  // }
+  res.render('pages/company', {
+    title: company?.name ?? 'Brak wyników',
+    name: company?.name,
+    companies
+  });
+});
 
+app.get('*', (req, res) => {
+  res.render('errors/404', {
+    title: 'Nie znaleziono',
+    layout: 'layouts/minimalistic'
+  });
 });
 
 app.listen(port);
